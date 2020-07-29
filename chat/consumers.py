091a6d3f34bf2ec05e2c606c818dab1200b8b221
alexalemanhdz <alex.aleman.public@gmail.com>
2020-07-29 12:44:54 -0500

    Initial commit

---
+++ chat/consumers.py
@@ -0,0 +1,33 @@
+from asgiref.sync import async_to_sync
+import json
+from channels.generic.websocket import WebsocketConsumer
+
+class chatConsumer(WebsocketConsumer):
+  def connect(self):
+    async_to_sync(self.channel_layer.group_add)(
+      'chat',
+      self.channel_name
+    )
+
+    self.accept()
+
+  def disconnect(self, close_code):
+    async_to_sync(self.channel_layer.group_discard)(
+      'chat',
+      self.channel_name
+    )
+
+  def receive(self, text_data):
+    text_data_json = json.loads(text_data)
+
+    async_to_sync(self.channel_layer.group_send)(
+      'chat',
+      {
+        'type': 'message',
+        'msg': text_data_json['msg'],
+        'user': text_data_json['user']
+      }
+    )
+
+  def message(self, event):
+    self.send(text_data=json.dumps(event))
